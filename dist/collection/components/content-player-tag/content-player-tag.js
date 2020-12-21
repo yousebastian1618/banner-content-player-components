dc45a9d821d3146bb06e39772a1cc809ddd55c84
Luke <lmiller@prismview.com>
2020-12-21 12:33:34 -0700

    add comments back in

--- dist/collection/components/content-player-tag/content-player-tag.js
+++ dist/collection/components/content-player-tag/content-player-tag.js
@@ -1,44 +1,83 @@
 export class ContentPlayerTag {
-    constructor() {
-        this.slides = [];
-        this.loadContents = (event) => {
-            this.el.style.opacity = "0";
-            this.slides = event.detail;
-        };
-        this.playNextContent = () => {
-            this.el.style.opacity = "1";
-        };
-    }
-    componentDidLoad() {
-        window.addEventListener("CONTENT_PLAYER_LOAD", this.loadContents);
-        window.addEventListener("CONTENT_PLAYER_CHANGED", this.playNextContent);
-    }
-    componentDidUnload() {
-        window.removeEventListener("CONTENT_PLAYER_LOAD", this.loadContents);
-        window.removeEventListener("CONTENT_PLAYER_CHANGED", this.playNextContent);
-        let closeEvent = new Event("CONTENT_PLAYER_CLOSE");
-        window.dispatchEvent(closeEvent);
-    }
-    render() {
-        return (h("div", { id: "content-player", class: "content-player-wrapper" }, this.slides.map((slideObject) => {
-            return (h("content-slide-tag", { contentSlideObject: slideObject }));
-        })));
-    }
-    static get is() { return "content-player-tag"; }
-    static get properties() { return {
-        "currentContentId": {
-            "type": String,
-            "attr": "current-content-id",
-            "mutable": true
-        },
-        "el": {
-            "elementRef": true
-        },
-        "slides": {
-            "type": "Any",
-            "attr": "slides",
-            "mutable": true
-        }
-    }; }
-    static get style() { return "/**style-placeholder:content-player-tag:**/"; }
+  constructor() {
+    this.slides = [];
+    /**
+     * Loads contents into the array of slides. Each content represents a slide
+     * Starts showing the current content
+     * @param {any} event
+     * @return {undefined}
+     */
+    this.loadContents = (event) => {
+      this.el.style.opacity = "0";
+      this.slides = event.detail;
+    };
+    /**
+     * Stops showing current content to play the next one
+     * @return {undefined}
+     */
+    this.playNextContent = () => {
+      this.el.style.opacity = "1";
+    };
+  }
+  /**
+   * ContentPlayerTag events
+   *
+   * Listens events:
+   *  CONTENT_PLAYER_LOAD
+   *  CONTENT_PLAYER_CHANGED
+   *
+   * Dispatches events:
+   *  CONTENT_PLAYER_CLOSE
+   */
+  /**
+   * Lifecycle method that is called once when the component is fully loaded
+   * and the first render() occurs.
+   *
+   * Adds event listeners for "CONTENT_PLAYER_CHANGED" and for "CONTENT_PLAYER_LOAD"
+   */
+  componentDidLoad() {
+    window.addEventListener("CONTENT_PLAYER_LOAD", this.loadContents);
+    window.addEventListener("CONTENT_PLAYER_CHANGED", this.playNextContent);
+  }
+  /**
+   * Removes event listeners for "CONTENT_PLAYER_LOAD" and for "CONTENT_PLAYER_CHANGED"
+   */
+  componentDidUnload() {
+    window.removeEventListener("CONTENT_PLAYER_LOAD", this.loadContents);
+    window.removeEventListener("CONTENT_PLAYER_CHANGED", this.playNextContent);
+    let closeEvent = new Event("CONTENT_PLAYER_CLOSE");
+    window.dispatchEvent(closeEvent);
+  }
+  render() {
+    return h(
+      "div",
+      { id: "content-player", class: "content-player-wrapper" },
+      this.slides.map((slideObject) => {
+        return h("content-slide-tag", { contentSlideObject: slideObject });
+      })
+    );
+  }
+  static get is() {
+    return "content-player-tag";
+  }
+  static get properties() {
+    return {
+      currentContentId: {
+        type: String,
+        attr: "current-content-id",
+        mutable: true,
+      },
+      el: {
+        elementRef: true,
+      },
+      slides: {
+        type: "Any",
+        attr: "slides",
+        mutable: true,
+      },
+    };
+  }
+  static get style() {
+    return "/**style-placeholder:content-player-tag:**/";
+  }
 }
