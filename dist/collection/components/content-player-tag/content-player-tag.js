ed6060d7d2276f56d4a1b65ce9a64f1868e9cdf8
Luke <lmiller@prismview.com>
2020-12-21 12:31:39 -0700

    Fix img scaling based on context

--- dist/collection/components/content-player-tag/content-player-tag.js
+++ dist/collection/components/content-player-tag/content-player-tag.js
@@ -1,47 +1,18 @@
 export class ContentPlayerTag {
     constructor() {
         this.slides = [];
-        /**
-         * Loads contents into the array of slides. Each content represents a slide
-         * Starts showing the current content
-         * @param {any} event
-         * @return {undefined}
-         */
         this.loadContents = (event) => {
             this.el.style.opacity = "0";
             this.slides = event.detail;
         };
-        /**
-         * Stops showing current content to play the next one
-         * @return {undefined}
-         */
         this.playNextContent = () => {
             this.el.style.opacity = "1";
         };
     }
-    /**
-     * ContentPlayerTag events
-     *
-     * Listens events:
-     *  CONTENT_PLAYER_LOAD
-     *  CONTENT_PLAYER_CHANGED
-     *
-     * Dispatches events:
-     *  CONTENT_PLAYER_CLOSE
-     */
-    /**
-     * Lifecycle method that is called once when the component is fully loaded
-     * and the first render() occurs.
-     *
-     * Adds event listeners for "CONTENT_PLAYER_CHANGED" and for "CONTENT_PLAYER_LOAD"
-     */
     componentDidLoad() {
         window.addEventListener("CONTENT_PLAYER_LOAD", this.loadContents);
         window.addEventListener("CONTENT_PLAYER_CHANGED", this.playNextContent);
     }
-    /**
-     * Removes event listeners for "CONTENT_PLAYER_LOAD" and for "CONTENT_PLAYER_CHANGED"
-     */
     componentDidUnload() {
         window.removeEventListener("CONTENT_PLAYER_LOAD", this.loadContents);
         window.removeEventListener("CONTENT_PLAYER_CHANGED", this.playNextContent);
@@ -49,7 +20,7 @@ export class ContentPlayerTag {
         window.dispatchEvent(closeEvent);
     }
     render() {
-        return (h("div", { class: "content-player-wrapper" }, this.slides.map((slideObject) => {
+        return (h("div", { id: "content-player", class: "content-player-wrapper" }, this.slides.map((slideObject) => {
             return (h("content-slide-tag", { contentSlideObject: slideObject }));
         })));
     }
